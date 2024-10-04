06400e7b91bc2c9df2964b5fda95804cc12e3458
Caio Ceretta <caioceretta@gmail.com>
2024-10-04 11:42:53 -0300

    feat: create products constant

---
+++ nest-introduction/project-nest/src/constands/index.ts
@@ -0,0 +1,31 @@
+import Product from 'src/product/product.entity';
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: 'Smartphone X Pro',
+    price: 2000,
+    description: 'Discover the power of the Smartphone X Pro',
+    imageUrl:
+      'https://images.unsplash.com/photo-1507955987999-df4864ee80d4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvbmV8ZW58MHx8MHx8fDA%3D',
+  },
+  {
+    id: 2,
+    name: 'Notebook UltraPro 15',
+    price: 3000,
+    description: 'The Notebook UltraPro 15 is developer`s first choice',
+    imageUrl:
+      'https://plus.unsplash.com/premium_photo-1681666713787-4396751a846a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bm90ZWJvb2slMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D',
+  },
+  {
+    id: 3,
+    name: 'Tablet MaxTab 10',
+    price: 1500,
+    description:
+      'Tablet MaxTab 10 offers us the combination of beauty and performance',
+    imageUrl:
+      'https://images.unsplash.com/photo-1464380573004-8ca85a08751a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8VGFibGV0fGVufDB8fDB8fHww',
+  },
+];
+
+export default products;
