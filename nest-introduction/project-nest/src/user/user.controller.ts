4681a16c8989f7aee4d7107a893da582bc547790
Caio Ceretta <caioceretta@gmail.com>
2024-10-05 00:01:51 -0300

    feat: add fetchAll and save methods to the user controller

--- nest-introduction/project-nest/src/user/user.controller.ts
+++ nest-introduction/project-nest/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CacheProvider } from 'src/db/cache';
+import User from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly cache: CacheProvider) {}
+
+  @Get()
+  async fetchAll() {
+    return this.cache.fetchAll;
+  }
+
+  @Post()
+  async save(@Body() user: User) {
+    return this.cache.save('user', user.id, user);
+  }
+}
