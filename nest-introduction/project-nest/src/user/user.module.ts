fd4049b0f854729b921dbfa45da924603650e819
Caio Ceretta <caioceretta@gmail.com>
2024-10-05 00:01:30 -0300

    feat: user module to import the DbModule

--- nest-introduction/project-nest/src/user/user.module.ts
+++ nest-introduction/project-nest/src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [UserController],
 })
 export class UserModule {}
