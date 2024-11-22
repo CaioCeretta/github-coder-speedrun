e6b614b53fc9c79740ceb187e385f7019f2760cd
Caio Ceretta <caioceretta@gmail.com>
2024-11-22 20:33:17 -0300

    fix: ValidarAlias controller method

--- convit3-digital/apps/backend/src/eventos/eventos.controller.ts
+++ convit3-digital/apps/backend/src/eventos/eventos.controller.ts
@@ -21,6 +21,26 @@ export class EventosController {
     }
   }
 
+  /* Here on this decorator we are passing in two attributes, the alias and the id, of the event, we are passing these two
+  because if we are updating an event and eventually validate if the alias is valid or not, it may exist on the db, but it
+  has to exist also in the id we are updating, the id and the alias have to be the same. If we have a alias that we chose
+  with a different id that has already been registered on the db, it means that this alias is not valid.
+  When we create a empty event in our app, we are already creating it with an id, so all events have the id created even
+  before the data is being persisted. 
+  We are testing first if there is an event with the same alias as the one being passed
+  If there isn't an event with this alias, it will be valid but it will also check if we are with the alias matched with
+  the right id, and we eventually will want to update the event or anything, this will also be a valid alias, because that
+  id, is the owner of that alias.
+  
+  */
+
+  @Get('/validar/:alias/:id')
+  async validarAlias(@Param('alias') alias: string, @Param('id') id: string) {
+    const evento = eventos.find((evento) => evento.alias === alias);
+
+    return { valido: !evento || evento.id === id };
+  }
+
   private serializar(evento: Evento) {
     if (!evento) {
       return null;
