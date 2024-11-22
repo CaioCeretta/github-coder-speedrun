9a40d0f93ea15d144d70a7d9f78a4aa6e2d802d2
Caio Ceretta <caioceretta@gmail.com>
2024-11-22 14:33:39 -0300

    echo: reset db and new requests

--- convit3-digital/apps/backend/src/eventos/eventos.controller.ts
+++ convit3-digital/apps/backend/src/eventos/eventos.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { Data, eventos, type Evento } from 'core';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Data, eventos, Id, type Evento } from 'core';
 
 @Controller('eventos')
 export class EventosController {
@@ -8,7 +8,24 @@ export class EventosController {
     return eventos.map(this.serializar);
   }
 
+  @Get(':idOuAlias')
+  async buscarEvento(@Param('idOuAlias') idOuAlias: string) {
+    const idValido = Id.valido(idOuAlias);
+
+    if (idValido) {
+      return this.serializar(eventos.find((evento) => evento.id === idOuAlias));
+    } else {
+      return this.serializar(
+        eventos.find((evento) => evento.alias === idOuAlias),
+      );
+    }
+  }
+
   private serializar(evento: Evento) {
+    if (!evento) {
+      return null;
+    }
+
     return {
       ...evento,
       data: Data.formatar(evento.data),
@@ -16,6 +33,10 @@ export class EventosController {
   }
 
   private deserializar(evento: any): Evento {
+    if (!evento) {
+      return null;
+    }
+
     return {
       ...evento,
       data: Data.desformatar(evento.data),
