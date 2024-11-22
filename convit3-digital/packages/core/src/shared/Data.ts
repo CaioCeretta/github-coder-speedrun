490ab63061bd89768e91f432f7345d23c7fe899a
Caio Ceretta <caioceretta@gmail.com>
2024-11-22 10:50:48 -0300

    doc: Explanation of the formatar function

--- convit3-digital/packages/core/src/shared/Data.ts
+++ convit3-digital/packages/core/src/shared/Data.ts
@@ -1,6 +1,13 @@
 export default class Data {
   /* Simplify the process of transforming a date to text and text to date */
   static formatar(data: Date): string {
+    /* Pad function explanation
+      the pad function receives a n of type number as an argument, then, transforms
+      that n into a string, this is because the padStart is a string function, and
+      the .padStart(2, "0"), certifies that the resulting string is at least, 2
+      chars long, otherwise, it will add "0" to the beginning
+    
+    */
     const pad = (n: number) => n.toString().padStart(2, "0")
 
     const d = data ?? new Date()
