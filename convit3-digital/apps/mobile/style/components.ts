a784bf21dbc9e420da5eb4f32aacd154cb93be8c
Caio Ceretta <caioceretta@gmail.com>
2024-11-24 17:17:16 -0300

    feat: update components

--- convit3-digital/apps/mobile/style/components.ts
+++ convit3-digital/apps/mobile/style/components.ts
@@ -1,18 +1,30 @@
 import { StyleSheet } from "react-native";
-import { bgRed500 } from "./utility";
+import {
+  bgBlue600,
+  flexRow,
+  gapX2,
+  justifyCenter,
+  px4,
+  py2,
+  roundedMd,
+} from "./utility";
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   centerGrow: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
-  botao: {
-    backgroundColor: bgRed500.backgroundColor,
-    padding: 10,
-    borderRadius: 5
-  }
-})
+  button: {
+    columnGap: gapX2.columnGap,
+    flexDirection: flexRow.flexDirection,
+    justifyContent: justifyCenter.justifyContent,
+    paddingHorizontal: px4.paddingHorizontal,
+    paddingVertical: py2.paddingVertical,
+    borderRadius: roundedMd.borderRadius,
+    backgroundColor: bgBlue600.backgroundColor,
+  },
+});
 
-export const centerGrow = styles.centerGrow
-export const botao = styles.botao;
+export const button = style.button;
+export const centerGrow = style.centerGrow;
