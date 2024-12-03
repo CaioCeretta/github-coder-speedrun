27053311f834b3b35a6e5ed6ced5b6e9cb03adbc
Caio Ceretta <caioceretta@gmail.com>
2024-12-02 23:38:24 -0300

    feat: Campo entrada and explanation

--- convit3-digital/apps/frontend/src/components/shared/CampoEntrada.tsx
+++ convit3-digital/apps/frontend/src/components/shared/CampoEntrada.tsx
@@ -18,7 +18,38 @@ export interface CamposEntradaProps extends React.InputHTMLAttributes<HTMLInputE
 export default function CampoEntrada(props: CamposEntradaProps) {
   return (
     <div className='flex flex-col gap-2 '>
-      <div className='flex flex-col'  >
+      {/*
+        At first glance, it might look a little unnecessary the creation of another div with similar stylings but there
+        are some key reasons
+
+        1. Organization
+
+        . This second div is used to group related elements, such as label and descrição. This separation makes the code
+        more readable and organized, as well as easier to maintain
+        . Without it, all the elements, label; descricao; input; erro; observacao, would be at the same level, which can
+        hinder understanding of the layout, specially in complex components
+
+        2. Styling Control
+
+        - If in the future, we find the necessity of applying different and specific styles, just for this group, the second
+        div will allow us to do it
+
+        3. Maintenance and Stability
+
+        - If we have the need to add more elements related to the label and descricao, such as an icon or a tooltip, we
+        already have a separated group.
+
+        4. Avoid inheritance of conflicts
+
+        - The use of flex in the external div can impact elements like input, erro and obscervação, that may not need this
+        disposition
+        - By encapsulating these two fields in a div, we isolate the application from the flex flex-col for these two specific
+        elements
+
+
+
+      */}
+      <div className='flex flex-col'>
       {props.label && <label>{props.label}</label>}
       {props.descricao && <p>{props.descricao}</p>}
       </div>
