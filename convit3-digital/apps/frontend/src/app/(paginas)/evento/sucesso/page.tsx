b1c50bd5d776359da81ab8132d46bc68c911055b
Caio Ceretta <caioceretta@gmail.com>
2024-12-06 21:43:36 -0300

    feat: clipboard component an success page

--- convit3-digital/apps/frontend/src/app/(paginas)/evento/sucesso/page.tsx
+++ convit3-digital/apps/frontend/src/app/(paginas)/evento/sucesso/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
+import AcessarViaQrCode from '@/components/evento/AcessarViaQrCode'
 import InformacoesEvento from '@/components/evento/InformacoesEvento'
+import CopiarClipboard from '@/components/shared/CopiarClipboard'
 import Janela from '@/components/shared/Janela'
 import useEvento from '@/data/hooks/useEvento'
+import { IconFingerprint, IconLink } from '@tabler/icons-react'
 import type { Evento } from 'core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function EventoSucesso() {
   const { evento } = useEvento()
 
+  const [urlAtual, setUrlAtual] = useState("");
+
+  useEffect(() => {
+    setUrlAtual(window.location.origin)
+  }, [])
   
 
   return evento ? (
@@ -23,6 +31,29 @@ function EventoSucesso() {
         esconderNome={true}
       />
 
+      <div className='flex gap-5 items-center py-6'>
+        <div className='flex-1 flex flex-col gap-5'>
+          <CopiarClipboard
+            icone={IconLink}
+            label={'Link para Convidar'}
+            texto={`${urlAtual}/convite/${evento.alias}`}
+          />
+          <CopiarClipboard
+            icone={IconLink}
+            label={'Link Administrador'}
+            texto={`${urlAtual}/evento/admin/${evento.id}`}
+          />
+          <CopiarClipboard
+            icone={IconFingerprint}
+            label={'Senha Administrador'}
+            texto={ evento.senha ?? ""}
+            observacao='Essa senha não será exibida novamente, então guarde-a com cuidado'
+          />
+        </div>
+
+        <AcessarViaQrCode evento={evento as Evento}/>
+
+      </div>
     </Janela>
   ) : null
 }
---
+++ convit3-digital/apps/frontend/src/components/shared/CopiarClipboard.tsx
@@ -0,0 +1,35 @@
+import { IconCopy } from "@tabler/icons-react";
+
+export interface CopiarClipboardProps {
+  label: string;
+  texto: string;
+  observacao?: string;
+  icone: React.ElementType
+}
+
+function CopiarClipboard(props: CopiarClipboardProps) {
+
+  function copiarTexto() {
+    navigator.clipboard.writeText(props.texto);
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+    <span>{props.label}</span>
+    <div className="flex items-center bg-black
+     border border-zinc-800 gap-3 px-4 py-2 text-zinc-300 text-lg">
+        {props.icone && <props.icone stroke={1.3} />}
+        <span className="flex-1">{props.texto}</span>
+        <IconCopy stroke={1.3} onClick={copiarTexto} className="cursor-pointer" />
+      </div>
+      {props.observacao && (
+        <span className="text-xs text-yellow-300/80">
+          {props.observacao}
+        </span>
+      )}
+    
+    </div>
+  )
+}
+
+export default CopiarClipboard
