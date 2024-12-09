2249c7fd56b08e8beaf3f9195264c80bd854f41a
Caio Ceretta <caioceretta@gmail.com>
2024-12-08 22:43:58 -0300

    feat: messages context

--- convit3-digital/apps/frontend/src/app/(paginas)/layout.tsx
+++ convit3-digital/apps/frontend/src/app/(paginas)/layout.tsx
@@ -1,12 +1,17 @@
 import Pagina from "@/components/template/Pagina";
+import { Toaster } from "@/components/ui/toaster";
 import { ProvedorContextoEvento } from "@/data/contexts/ContextoEventos";
+import { ProvedorContextoMensagem } from "@/data/contexts/ContextoMensagens";
 
 export default function Layout(props: any) {
   return (
-    <ProvedorContextoEvento>
-        <Pagina>
-          {props.children}
-        </Pagina>
-    </ProvedorContextoEvento >
+    <ProvedorContextoMensagem>
+      <ProvedorContextoEvento>
+          <Pagina>
+            {props.children}
+          </Pagina>
+          <Toaster />
+      </ProvedorContextoEvento>
+    </ProvedorContextoMensagem>
   )
 }
---
+++ convit3-digital/apps/frontend/src/data/contexts/ContextoMensagens.tsx
@@ -0,0 +1,37 @@
+import { useToast } from "@/data/hooks/useToast";
+import { createContext, useCallback } from "react";
+
+export interface ContextoMensagemProps {
+  adicionarSucesso: (texto: string) => void
+  adicionarErro: (texto: string) => void
+}
+
+const ContextoMensagens = createContext<ContextoMensagemProps>({} as any);
+
+export function ProvedorContextoMensagem(props: any) {
+  const { toast } = useToast()
+
+  const adicionarMensagem = useCallback((tipo: 'sucesso' | 'erro', texto: string) => {
+    toast({
+      title: tipo === 'sucesso' ? 'Tudo certo por aqui' : 'Ops, algo deu errado',
+      description: texto,
+      variant: tipo === 'sucesso' ? 'default' : 'destructive'
+
+    })
+  }, [toast]);
+
+  return (
+      <ContextoMensagens.Provider value={{
+        adicionarSucesso(texto) { 
+          adicionarMensagem('sucesso', texto)
+        },
+        adicionarErro(texto) {
+          adicionarMensagem('erro', texto)
+        }
+      }}>
+        {props.children }
+      </ContextoMensagens.Provider>
+    )
+}
+
+export default ContextoMensagens
