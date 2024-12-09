17cdf0d80674c61c7d34f4395eb7dbf4ed875040
Caio Ceretta <caioceretta@gmail.com>
2024-12-08 21:43:35 -0300

    feat: added the simnao component for the guests

--- convit3-digital/apps/frontend/src/components/evento/FormConvidado.tsx
+++ convit3-digital/apps/frontend/src/components/evento/FormConvidado.tsx
@@ -1,5 +1,6 @@
 import type { Convidado } from "core"
 import CampoEntrada from "../shared/CampoEntrada"
+import CampoSimNao from "../shared/CampoSimNao"
 
 export interface FormConvidadoProps {
   convidado: Partial<Convidado>
@@ -23,10 +24,53 @@ function FormConvidado(props: FormConvidadoProps) {
           props.convidadoMudou({...props.convidado, email: e.target.value})
         }
       />
-
+      <div className="gap-5 flex">
+        <CampoSimNao
+          label="Presença Confirmada?"
+          value={props.convidado.confirmado ?? true}
+          onChange={(valor) => 
+            props.convidadoMudou({
+              ...props.convidado,
+              confirmado: valor
+            })
+          }
+          className="flex-1"
+        />
+      {props.convidado.confirmado && (
+        <div className="flex gap-5">
+        <CampoSimNao
+          label="Possui Acompanhantes?"
+          value={props.convidado.possuiAcompanhantes ?? false}
+          onChange={(valor) => 
+            props.convidadoMudou({
+              ...props.convidado,
+              possuiAcompanhantes: valor,
+              qtdeAcompanhantes: valor ? 1 : 0
+            })
+          }
+          className="flex-1"
+          />
+      {props.convidado.possuiAcompanhantes && (
+        <CampoEntrada
+          label="Quantos Acompanhantes?"
+          value={props.convidado.qtdeAcompanhantes ?? 1}
+          onChange={
+            (e) =>
+              props.convidadoMudou({
+                ...props.convidado,
+                qtdeAcompanhantes: +e.target.value
+              })
+          }
+          min={1}          
+          type="number"
+          />
+      )}
+      </div>
+      )}
 
 
         
+      </div>
     </div>
   )
 }
