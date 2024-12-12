139ff2296ba0c7275d2a81a9ec12eb140c9dec5f
Caio Ceretta <caioceretta@gmail.com>
2024-12-12 14:35:20 -0300

    feat: add footer

--- ordem-livros/ordem-livros/src/components/template/Pagina.svelte
+++ ordem-livros/ordem-livros/src/components/template/Pagina.svelte
@@ -1,13 +1,15 @@
 <script>
   import Cabecalho from "./Cabecalho.svelte";
+  import Rodape from "./Rodape.svelte";
 </script>
 
 <div class="flex flex-col min-h-screen bg-black text-white">
   <Cabecalho />
-  <main>
+  <main class="flex-1">
     <slot></slot>
     <!-- The slot component works the same as the outlet in react, so if in our layout we have a page enclosing the children
      the children won't be shown whenever we enter a page with that layout, because for it to show we need to use the <slot>
      component -->
   </main>
+  <Rodape />
 </div>
---
+++ ordem-livros/ordem-livros/src/components/template/Rodape.svelte
@@ -0,0 +1,11 @@
+<div
+  class="h-16 bg-zinc-800
+  flex justify-end items-center
+  text-lg font-black px-20
+  border-zinc-800 border-t
+"
+>
+  <span class="font-thin text-zinc-400"
+    >Todos os direitos reservados &copy; {new Date().getFullYear()}</span
+  >
+</div>
