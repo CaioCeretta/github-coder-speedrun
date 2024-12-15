2fe4a376dbceedfe57d95d45108495eb45ad63f4
Caio Ceretta <caioceretta@gmail.com>
2024-12-15 00:00:25 -0300

    feat: up to authors page

--- ordem-livros/ordem-livros/src/routes/autores/+page.svelte
+++ ordem-livros/ordem-livros/src/routes/autores/+page.svelte
@@ -1,7 +1,19 @@
 <script>
-  import Caixa from "../../components/shared/Caixa.svelte";
+	import ItemAutor from '../../components/author/ItemAutor.svelte'
+	import Titulo from '../../components/shared/Titulo.svelte'
+
+	import autores from '../../constants/autores'
 </script>
 
-<h1 class="text-3xl font-bold">Autores</h1>
+<div class="flex flex-col gap-5">
+	<Titulo
+		principal="Autores"
+		secundario="Conheça os autores que escrevem as histórias que você ama!"
+	/>
 
-<a href="/autores/george-martin">George Martin</a>
+	<div class="py-3 grid grid-cols-2 gap-5">
+		{#each autores as autor}
+			<ItemAutor {autor} />
+		{/each}
+	</div>
+</div>
